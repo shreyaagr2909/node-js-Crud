1223c86acbb8d1addd09c4497d75a8911ed85a19
Shreya Agarwal <46715038+shreyaagr2909@users.noreply.github.com>
2019-06-27 14:44:12 +0530

    Update databaseConnection.js

--- mysql updated/databaseConnection.js
+++ mysql updated/databaseConnection.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const con = mysql.createConnection({
     host : 'localhost' , 
     user : 'root' ,
-    password : 'root' ,
-    database : 'studentnew'
+    password : ' ' ,
+    database : ' '
 });
 
 module.exports = con;
